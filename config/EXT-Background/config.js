71bf9768bc84a26c6e7828013c3d906ff416c8cd
bugsounet <github@bugsounet.fr>
2022-04-24 23:37:14 +0200

    add schema of Alert, background, bring

--- config/EXT-Background/config.js
+++ config/EXT-Background/config.js
@@ -8,7 +8,40 @@ var defaultConfig = {
 }
 
 var schema = {
-
+  "title": "EXT-Background",
+  "description": "Properties for EXT-Background plugin",
+  "type": "object",
+  "properties": {
+    "module": {
+      "type": "string",
+      "title": "Plugin name",
+      "default": "EXT-Background"
+    },
+    "disabled": {
+      "type": "boolean",
+      "title": "Disable the plugin",
+      "default": false
+    },
+    "config": {
+      "type": "object",
+      "title": "configuration",
+      "properties": {
+        "model": {
+          "type": "string",
+          "title": "Choose your model",
+          "default": "jarvis",
+          "enum": ["jarvis", "lego", "old", "cortana"]
+        },
+        "myImage": {
+          "type": ["string", "null"],
+          "title": "Choose your personal image, if don't don't want model",
+          "default": null,
+        },
+      },
+      "required": ["model"]
+    }
+  },
+  "required": ["config","module"]
 }
 
 exports.default = defaultConfig
