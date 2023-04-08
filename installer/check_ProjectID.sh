ec7dc1a8ba61474cd203f5abaca7641224f92efe
bugsounet <bugsounet@bugsounet.fr>
2023-04-08 16:32:00 +0200

    check SmartHome project_id too

--- installer/check_ProjectID.sh
+++ installer/check_ProjectID.sh
@@ -1,5 +1,10 @@
-echo "Your project_id is: \c"
+echo "Your MMM-GoogleAssistant project_id is: \c"
 cat ../MMM-GoogleAssistant/credentials.json 2>/dev/null | grep -oP '(?<="project_id":")[^"]*' || {
+  echo "credentials not found!"
+}
+echo "---"
+echo "Your SmartHome project_id is: \c"
+cat credentials.json 2>/dev/null | grep -oP '(?<="project_id": ")[^"]*' || {
   echo "credentials not found!"
 }
 echo "\n"
