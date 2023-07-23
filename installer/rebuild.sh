525c31584d2f4131d36a91b94ba30faf1e558ee9
bugsounet <github@bugsounet.fr>
2023-07-24 01:06:08 +0200

    rebuilded with InstallerCore

--- installer/rebuild.sh
+++ installer/rebuild.sh
@@ -23,20 +23,26 @@ source utils.sh
 # Go back to module root
 cd ..
 
-Installer_info "Welcome to Gateway rebuild script"
+# check version in package.json file
+Installer_version="$(grep -Eo '\"version\"[^,]*' ./package.json | grep -Eo '[^:]*$' | awk  -F'\"' '{print $2}')"
+Installer_module="$(grep -Eo '\"name\"[^,]*' ./package.json | grep -Eo '[^:]*$' | awk  -F'\"' '{print $2}')"
+
+# Let's start !
+Installer_info "Welcome to $Installer_module v$Installer_version rebuild script"
 Installer_warning "This script will erase current build and reinstall it"
-#Installer_yesno "Do you want to continue ?" || exit 0
+Installer_yesno "Do you want to continue ?" || exit 0
 
 echo
 Installer_info "Deleting: package-lock.json node_modules" 
 rm -rf package-lock.json node_modules
 Installer_success "Done."
 
-echo
-Installer_info "Upgrading Gateway..."
-git reset --hard HEAD
-git pull
-Installer_success "Done."
+Installer_info "Updating..."
+(git reset --hard && git pull) || {
+  Installer_error "Update Failed!"
+  exit 255
+}
+Installer_success "Done"
 
-Installer_info "Reinstalling Gateway..."
+Installer_info "Reinstalling..."
 npm install
