521b1f934f3246c25e74332f1683937aa63e987d
bugsounet <bugsounet@bugsounet.fr>
2023-10-22 12:54:23 +0200

    add CPU/MEMORY

--- components/adminPage.js
+++ components/adminPage.js
@@ -100,7 +100,7 @@ class adminPageGW {
         var Hostname_container = document.createElement("div")
         Hostname_container.id = "GATEWAY_ADMIN-HOSTNAME_CONTAINER"
         content_wrapper.appendChild(Hostname_container)
-        
+
           var Hostname_value = document.createElement("div")
           Hostname_value.id = "GATEWAY_ADMIN-HOSTNAME_VALUE"
           Hostname_value.textContent = "Loading"
@@ -113,7 +113,8 @@ class adminPageGW {
           var Sysinfo_container_row = document.createElement("div")
           Sysinfo_container_row.id = "GATEWAY_ADMIN-SYSINFO_CONTAINER_ROW"
           Sysinfo_container.appendChild(Sysinfo_container_row)
-          
+
+            /** Versions **/
             var Sysinfo_version = document.createElement("div")
             Sysinfo_version.id = "GATEWAY_ADMIN-SYSINFO_VERSION"
             Sysinfo_container_row.appendChild(Sysinfo_version)
@@ -135,6 +136,70 @@ class adminPageGW {
                 Sysinfo_version_list.id = "GATEWAY_ADMIN-SYSINFO_VERSION_LIST"
                 Sysinfo_version_group.appendChild(Sysinfo_version_list)
 
+			        var MM = document.createElement("div")
+			        MM.id = "GATEWAY_ADMIN-SYSINFO_VERSION-MM"
+			        MM.textContent = "MagicMirror²:"
+			        Sysinfo_version_list.appendChild(MM)
+			          var MM_Value = document.createElement("div")
+			          MM_Value.id = "GATEWAY_ADMIN-SYSINFO_VERSION-MM-VALUE"
+			          MM_Value.textContent= this.System.VERSION.MagicMirror
+			          MM.appendChild(MM_Value)
+
+			        var ELECTRON = document.createElement("div")
+			        ELECTRON.id = "GATEWAY_ADMIN-SYSINFO_VERSION-ELECTRON"
+			        ELECTRON.textContent = "Electron:"
+			        Sysinfo_version_list.appendChild(ELECTRON)
+			          var ELECTRON_Value = document.createElement("div")
+			          ELECTRON_Value.id = "GATEWAY_ADMIN-SYSINFO_VERSION-ELECTRON-VALUE"
+			          ELECTRON_Value.textContent= this.System.VERSION.ELECTRON
+			          ELECTRON.appendChild(ELECTRON_Value)
+
+			        var MMNode = document.createElement("div")
+			        MMNode.id = "GATEWAY_ADMIN-SYSINFO_VERSION-MMNODE"
+			        MMNode.textContent = "MagicMirrror² Node:"
+			        Sysinfo_version_list.appendChild(MMNode)
+			          var MMNode_Value = document.createElement("div")
+			          MMNode_Value.id = "GATEWAY_ADMIN-SYSINFO_VERSION-MMNODE-VALUE"
+			          MMNode_Value.textContent= this.System.VERSION.NODEMM
+			          MMNode.appendChild(MMNode_Value)
+
+			        var Node = document.createElement("div")
+			        Node.id = "GATEWAY_ADMIN-SYSINFO_VERSION-NODE"
+			        Node.textContent = translate["System_NodeVersion"]
+			        Sysinfo_version_list.appendChild(Node)
+			          var Node_Value = document.createElement("div")
+			          Node_Value.id = "GATEWAY_ADMIN-SYSINFO_VERSION-NODE-VALUE"
+			          Node_Value.textContent= this.System.VERSION.NODECORE
+			          Node.appendChild(Node_Value)
+
+			        var NPM = document.createElement("div")
+			        NPM.id = "GATEWAY_ADMIN-SYSINFO_VERSION-NPM"
+			        NPM.textContent = translate["System_NPMVersion"]
+			        Sysinfo_version_list.appendChild(NPM)
+			          var NPM_Value = document.createElement("div")
+			          NPM_Value.id = "GATEWAY_ADMIN-SYSINFO_VERSION-NPM-VALUE"
+			          NPM_Value.textContent= this.System.VERSION.NPM
+			          NPM.appendChild(NPM_Value)
+
+			        var OS = document.createElement("div")
+			        OS.id = "GATEWAY_ADMIN-SYSINFO_VERSION-OS"
+			        OS.textContent = translate["System_OSVersion"]
+			        Sysinfo_version_list.appendChild(OS)
+			          var OS_Value = document.createElement("div")
+			          OS_Value.id = "GATEWAY_ADMIN-SYSINFO_VERSION-OS-VALUE"
+			          OS_Value.textContent= this.System.VERSION.OS
+			          OS.appendChild(OS_Value)
+
+			        var Kernel = document.createElement("div")
+			        Kernel.id = "GATEWAY_ADMIN-SYSINFO_VERSION-KERNEL"
+			        Kernel.textContent = translate["System_KernelVersion"]
+			        Sysinfo_version_list.appendChild(Kernel)
+			          var Kernel_Value = document.createElement("div")
+			          Kernel_Value.id = "GATEWAY_ADMIN-SYSINFO_VERSION-KERNEL-VALUE"
+			          Kernel_Value.textContent= this.System.VERSION.KERNEL
+			          Kernel.appendChild(Kernel_Value)
+
+            /** CPU **/
             var Sysinfo_cpu = document.createElement("div")
             Sysinfo_cpu.id = "GATEWAY_ADMIN-SYSINFO_CPU"
             Sysinfo_container_row.appendChild(Sysinfo_cpu)
@@ -156,6 +221,52 @@ class adminPageGW {
                 Sysinfo_cpu_list.id = "GATEWAY_ADMIN-SYSINFO_CPU_LIST"
                 Sysinfo_cpu_group.appendChild(Sysinfo_cpu_list)
 
+			        var Type = document.createElement("div")
+			        Type.id = "GATEWAY_ADMIN-SYSINFO_CPU-TYPE"
+			        Type.textContent = translate["System_TypeCPU"]
+			        Sysinfo_cpu_list.appendChild(Type)
+			          var Type_Value = document.createElement("div")
+			          Type_Value.id = "GATEWAY_ADMIN-SYSINFO_CPU-TYPE-VALUE"
+			          Type_Value.textContent= this.System.CPU.type
+			          Type.appendChild(Type_Value)
+
+			        var Speed = document.createElement("div")
+			        Speed.id = "GATEWAY_ADMIN-SYSINFO_CPU-SPEED"
+			        Speed.textContent = translate["System_SpeedCPU"]
+			        Sysinfo_cpu_list.appendChild(Speed)
+			          var Speed_Value = document.createElement("div")
+			          Speed_Value.id = "GATEWAY_ADMIN-SYSINFO_CPU-SPEED-VALUE"
+			          Speed_Value.textContent= this.System.CPU.speed
+			          Speed.appendChild(Speed_Value)
+
+			        var Usage = document.createElement("div")
+			        Usage.id = "GATEWAY_ADMIN-SYSINFO_CPU-USAGE"
+			        Usage.textContent = translate["System_CurrentLoadCPU"]
+			        Sysinfo_cpu_list.appendChild(Usage)
+			          var Usage_Value = document.createElement("div")
+			          Usage_Value.id = "GATEWAY_ADMIN-SYSINFO_CPU-USAGE-VALUE"
+			          Usage_Value.textContent= this.System.CPU.usage
+			          Usage.appendChild(Usage_Value)
+
+			        var Governor = document.createElement("div")
+			        Governor.id = "GATEWAY_ADMIN-SYSINFO_CPU-GOVERNOR"
+			        Governor.textContent = translate["System_GovernorCPU"]
+			        Sysinfo_cpu_list.appendChild(Governor)
+			          var Governor_Value = document.createElement("div")
+			          Governor_Value.id = "GATEWAY_ADMIN-SYSINFO_CPU-GOVERNOR-VALUE"
+			          Governor_Value.textContent= this.System.CPU.governor
+			          Governor.appendChild(Governor_Value)
+
+			        var Temp = document.createElement("div")
+			        Temp.id = "GATEWAY_ADMIN-SYSINFO_CPU-TEMP"
+			        Temp.textContent = translate["System_TempCPU"]
+			        Sysinfo_cpu_list.appendChild(Temp)
+			          var Temp_Value = document.createElement("div")
+			          Temp_Value.id = "GATEWAY_ADMIN-SYSINFO_CPU-TEMP-VALUE"
+			          Temp_Value.textContent= this.System.CPU.temp.C
+			          Temp.appendChild(Temp_Value)
+
+            /** Memory **/
             var Sysinfo_memory = document.createElement("div")
             Sysinfo_memory.id = "GATEWAY_ADMIN-SYSINFO_MEMORY"
             Sysinfo_container_row.appendChild(Sysinfo_memory)
@@ -177,6 +288,25 @@ class adminPageGW {
                 Sysinfo_memory_list.id = "GATEWAY_ADMIN-SYSINFO_MEMORY_LIST"
                 Sysinfo_memory_group.appendChild(Sysinfo_memory_list)
 
+			        var Active = document.createElement("div")
+			        Active.id = "GATEWAY_ADMIN-SYSINFO_MEMORY-ACTIVE"
+			        Active.textContent = translate["System_TypeMemory"]
+			        Sysinfo_memory_list.appendChild(Active)
+			          var Active_Value = document.createElement("div")
+			          Active_Value.id = "GATEWAY_ADMIN-SYSINFO_MEMORY-ACTIVE-VALUE"
+			          Active_Value.textContent= this.System.MEMORY.used + " / " + this.System.MEMORY.total + " ("+ this.System.MEMORY.percent + "%)"
+			          Active.appendChild(Active_Value)
+
+			        var Swap = document.createElement("div")
+			        Swap.id = "GATEWAY_ADMIN-SYSINFO_MEMORY-SWAP"
+			        Swap.textContent = translate["System_SwapMemory"]
+			        Sysinfo_memory_list.appendChild(Swap)
+			          var Swap_Value = document.createElement("div")
+			          Swap_Value.id = "GATEWAY_ADMIN-SYSINFO_MEMORY-SWAP-VALUE"
+			          Swap_Value.textContent= this.System.MEMORY.swapUsed + " / " + this.System.MEMORY.swapTotal + " ("+ this.System.MEMORY.swapPercent + "%)"
+			          Swap.appendChild(Swap_Value)
+
+            /** Network **/
             var Sysinfo_network = document.createElement("div")
             Sysinfo_network.id = "GATEWAY_ADMIN-SYSINFO_NETWORK"
             Sysinfo_container_row.appendChild(Sysinfo_network)
@@ -244,157 +374,6 @@ class adminPageGW {
     document.body.appendChild(wrapper)
     this.init=true
     console.log("[Gateway] AdminPage Ready !")
-
-
-    return
-    
-    var SysInfo=document.createElement("div")
-    SysInfo.id="GATEWAY_ADMIN"
-
-      /** Version **/
-      var Version = document.createElement("div")
-      Version.id = "GATEWAY_VERSION"
-      Version.textContent = translate["System"]
-        var MM = document.createElement("div")
-        MM.id = "GATEWAY_VERSION-MM"
-        MM.textContent = "MagicMirror²:"
-        Version.appendChild(MM)
-          var MM_Value = document.createElement("div")
-          MM_Value.id = "GATEWAY_VERSION-MM-VALUE"
-          MM_Value.textContent= this.System.VERSION.MagicMirror
-          MM.appendChild(MM_Value)
-
-        var ELECTRON = document.createElement("div")
-        ELECTRON.id = "GATEWAY_VERSION-ELECTRON"
-        ELECTRON.textContent = "Electron:"
-        Version.appendChild(ELECTRON)
-          var ELECTRON_Value = document.createElement("div")
-          ELECTRON_Value.id = "GATEWAY_VERSION-ELECTRON-VALUE"
-          ELECTRON_Value.textContent= this.System.VERSION.ELECTRON
-          ELECTRON.appendChild(ELECTRON_Value)
-
-        var MMNode = document.createElement("div")
-        MMNode.id = "GATEWAY_VERSION-MMNODE"
-        MMNode.textContent = "MagicMirrror² Node:"
-        Version.appendChild(MMNode)
-          var MMNode_Value = document.createElement("div")
-          MMNode_Value.id = "GATEWAY_VERSION-MMNODE-VALUE"
-          MMNode_Value.textContent= this.System.VERSION.NODEMM
-          MMNode.appendChild(MMNode_Value)
-
-        var Node = document.createElement("div")
-        Node.id = "GATEWAY_VERSION-NODE"
-        Node.textContent = translate["System_NodeVersion"]
-        Version.appendChild(Node)
-          var Node_Value = document.createElement("div")
-          Node_Value.id = "GATEWAY_VERSION-NODE-VALUE"
-          Node_Value.textContent= this.System.VERSION.NODECORE
-          Node.appendChild(Node_Value)
-
-        var NPM = document.createElement("div")
-        NPM.id = "GATEWAY_VERSION-NPM"
-        NPM.textContent = translate["System_NPMVersion"]
-        Version.appendChild(NPM)
-          var NPM_Value = document.createElement("div")
-          NPM_Value.id = "GATEWAY_VERSION-NPM-VALUE"
-          NPM_Value.textContent= this.System.VERSION.NPM
-          NPM.appendChild(NPM_Value)
-
-        var OS = document.createElement("div")
-        OS.id = "GATEWAY_VERSION-OS"
-        OS.textContent = translate["System_OSVersion"]
-        Version.appendChild(OS)
-          var OS_Value = document.createElement("div")
-          OS_Value.id = "GATEWAY_VERSION-OS-VALUE"
-          OS_Value.textContent= this.System.VERSION.OS
-          OS.appendChild(OS_Value)
-
-        var Kernel = document.createElement("div")
-        Kernel.id = "GATEWAY_VERSION-KERNEL"
-        Kernel.textContent = translate["System_KernelVersion"]
-        Version.appendChild(Kernel)
-          var Kernel_Value = document.createElement("div")
-          Kernel_Value.id = "GATEWAY_VERSION-KERNEL-VALUE"
-          Kernel_Value.textContent= this.System.VERSION.KERNEL
-          Kernel.appendChild(Kernel_Value)
-      SysInfo.appendChild(Version)
-
-      /** CPU **/
-      var CPU = document.createElement("div")
-      CPU.id = "GATEWAY_CPU"
-      CPU.textContent = translate["System_CPUSystem"]
-        var Type = document.createElement("div")
-        Type.id = "GATEWAY_CPU-TYPE"
-        Type.textContent = translate["System_TypeCPU"]
-        CPU.appendChild(Type)
-          var Type_Value = document.createElement("div")
-          Type_Value.id = "GATEWAY_CPU-TYPE-VALUE"
-          Type_Value.textContent= this.System.CPU.type
-          Type.appendChild(Type_Value)
-
-        var Speed = document.createElement("div")
-        Speed.id = "GATEWAY_CPU-SPEED"
-        Speed.textContent = translate["System_SpeedCPU"]
-        CPU.appendChild(Speed)
-          var Speed_Value = document.createElement("div")
-          Speed_Value.id = "GATEWAY_CPU-SPEED-VALUE"
-          Speed_Value.textContent= this.System.CPU.speed
-          Speed.appendChild(Speed_Value)
-
-        var Usage = document.createElement("div")
-        Usage.id = "GATEWAY_CPU-USAGE"
-        Usage.textContent = translate["System_CurrentLoadCPU"]
-        CPU.appendChild(Usage)
-          var Usage_Value = document.createElement("div")
-          Usage_Value.id = "GATEWAY_CPU-USAGE-VALUE"
-          Usage_Value.textContent= this.System.CPU.usage
-          Usage.appendChild(Usage_Value)
-
-        var Governor = document.createElement("div")
-        Governor.id = "GATEWAY_CPU-GOVERNOR"
-        Governor.textContent = translate["System_GovernorCPU"]
-        CPU.appendChild(Governor)
-          var Governor_Value = document.createElement("div")
-          Governor_Value.id = "GATEWAY_CPU-GOVERNOR-VALUE"
-          Governor_Value.textContent= this.System.CPU.governor
-          Governor.appendChild(Governor_Value)
-
-        var Temp = document.createElement("div")
-        Temp.id = "GATEWAY_CPU-TEMP"
-        Temp.textContent = translate["System_TempCPU"]
-        CPU.appendChild(Temp)
-          var Temp_Value = document.createElement("div")
-          Temp_Value.id = "GATEWAY_CPU-TEMP-VALUE"
-          Temp_Value.textContent= this.System.CPU.temp.C
-          Temp.appendChild(Temp_Value)
-      SysInfo.appendChild(CPU)
-
-      /** Memory **/
-      var MEMORY = document.createElement("div")
-      MEMORY.id = "GATEWAY_MEMORY"
-      MEMORY.textContent = translate["System_MemorySystem"]
-        var Active = document.createElement("div")
-        Active.id = "GATEWAY_MEMORY-ACTIVE"
-        Active.textContent = translate["System_TypeMemory"]
-        MEMORY.appendChild(Active)
-          var Active_Value = document.createElement("div")
-          Active_Value.id = "GATEWAY_MEMORY-ACTIVE-VALUE"
-          Active_Value.textContent= this.System.MEMORY.used + " / " + this.System.MEMORY.total + " ("+ this.System.MEMORY.percent + "%)"
-          Active.appendChild(Active_Value)
-
-        var Swap = document.createElement("div")
-        Swap.id = "GATEWAY_MEMORY-SWAP"
-        Swap.textContent = translate["System_SwapMemory"]
-        MEMORY.appendChild(Swap)
-          var Swap_Value = document.createElement("div")
-          Swap_Value.id = "GATEWAY_MEMORY-SWAP-VALUE"
-          Swap_Value.textContent= this.System.MEMORY.swapUsed + " / " + this.System.MEMORY.swapTotal + " ("+ this.System.MEMORY.swapPercent + "%)"
-          Swap.appendChild(Swap_Value)
-      SysInfo.appendChild(MEMORY)
-
-    document.body.appendChild(SysInfo)
-    this.init=true
-    console.log("[Gateway] AdminPage Ready !")
   }
 
   show() {
@@ -433,37 +412,39 @@ class adminPageGW {
   refreshData() {
     var Hostname_value = document.getElementById("GATEWAY_ADMIN-HOSTNAME_VALUE")
     Hostname_value.textContent = this.System.HOSTNAME
-    return
+
     /* Version */
-    var MM_Value = document.getElementById("GATEWAY_VERSION-MM-VALUE")
+    var MM_Value = document.getElementById("GATEWAY_ADMIN-SYSINFO_VERSION-MM-VALUE")
     MM_Value.textContent = this.System.VERSION.MagicMirror
-    var ELECTRON_Value = document.getElementById("GATEWAY_VERSION-ELECTRON-VALUE")
+    var ELECTRON_Value = document.getElementById("GATEWAY_ADMIN-SYSINFO_VERSION-ELECTRON-VALUE")
     ELECTRON_Value.textContent = this.System.VERSION.ELECTRON
-    var MMNode_Value = document.getElementById("GATEWAY_VERSION-MMNODE-VALUE")
+    var MMNode_Value = document.getElementById("GATEWAY_ADMIN-SYSINFO_VERSION-MMNODE-VALUE")
     MMNode_Value.textContent = this.System.VERSION.NODEMM
-    var Node_Value = document.getElementById("GATEWAY_VERSION-NODE-VALUE")
+    var Node_Value = document.getElementById("GATEWAY_ADMIN-SYSINFO_VERSION-NODE-VALUE")
     Node_Value.textContent = this.System.VERSION.NODECORE
-    var NPM_Value = document.getElementById("GATEWAY_VERSION-NPM-VALUE")
+    var NPM_Value = document.getElementById("GATEWAY_ADMIN-SYSINFO_VERSION-NPM-VALUE")
     NPM_Value.textContent = this.System.VERSION.NPM
-    var OS_Value = document.getElementById("GATEWAY_VERSION-OS-VALUE")
+    var OS_Value = document.getElementById("GATEWAY_ADMIN-SYSINFO_VERSION-OS-VALUE")
     OS_Value.textContent = this.System.VERSION.OS
-    var Kernel_Value = document.getElementById("GATEWAY_VERSION-KERNEL-VALUE")
+    var Kernel_Value = document.getElementById("GATEWAY_ADMIN-SYSINFO_VERSION-KERNEL-VALUE")
     Kernel_Value.textContent = this.System.VERSION.KERNEL
+
     /* CPU */
-    var Type_Value = document.getElementById("GATEWAY_CPU-TYPE-VALUE")
+    var Type_Value = document.getElementById("GATEWAY_ADMIN-SYSINFO_CPU-TYPE-VALUE")
     Type_Value.textContent = this.System.CPU.type
-    var Speed_Value = document.getElementById("GATEWAY_CPU-SPEED-VALUE")
+    var Speed_Value = document.getElementById("GATEWAY_ADMIN-SYSINFO_CPU-SPEED-VALUE")
     Speed_Value.textContent= this.System.CPU.speed
-    var Usage_Value = document.getElementById("GATEWAY_CPU-USAGE-VALUE")
+    var Usage_Value = document.getElementById("GATEWAY_ADMIN-SYSINFO_CPU-USAGE-VALUE")
     Usage_Value.textContent= this.System.CPU.usage + "%"
-    var Governor_Value = document.getElementById("GATEWAY_CPU-GOVERNOR-VALUE")
+    var Governor_Value = document.getElementById("GATEWAY_ADMIN-SYSINFO_CPU-GOVERNOR-VALUE")
     Governor_Value.textContent= this.System.CPU.governor
-    var Temp_Value = document.getElementById("GATEWAY_CPU-TEMP-VALUE")
+    var Temp_Value = document.getElementById("GATEWAY_ADMIN-SYSINFO_CPU-TEMP-VALUE")
     Temp_Value.textContent = this.System.CPU.temp.C + "°"
+
     /* MEMORY */
-    var Active_Value = document.getElementById("GATEWAY_MEMORY-ACTIVE-VALUE")
+    var Active_Value = document.getElementById("GATEWAY_ADMIN-SYSINFO_MEMORY-ACTIVE-VALUE")
     Active_Value.textContent = this.System.MEMORY.used + " / " + this.System.MEMORY.total + " ("+ this.System.MEMORY.percent + "%)"
-    var Swap_Value = document.getElementById("GATEWAY_MEMORY-SWAP-VALUE")
+    var Swap_Value = document.getElementById("GATEWAY_ADMIN-SYSINFO_MEMORY-SWAP-VALUE")
     Swap_Value.textContent= this.System.MEMORY.swapUsed + " / " + this.System.MEMORY.swapTotal + " ("+ this.System.MEMORY.swapPercent + "%)"
   }
 
