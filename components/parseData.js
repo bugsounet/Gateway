1506b474877ead027961ae8f0f52c1f5ea501979
bugsounet <github@bugsounet.fr>
2023-12-06 20:33:59 +0100

    prepare for GA v6

--- components/parseData.js
+++ components/parseData.js
@@ -95,5 +95,23 @@ async function parse(that,data) {
   }
 }
 
+/** MM² cmpVersions function **/
+function cmpVersions(a, b) {
+  let i, diff;
+  const regExStrip0 = /(\.0+)+$/;
+  const segmentsA = a.replace(regExStrip0, "").split(".");
+  const segmentsB = b.replace(regExStrip0, "").split(".");
+  const l = Math.min(segmentsA.length, segmentsB.length);
+
+  for (i = 0; i < l; i++) {
+    diff = parseInt(segmentsA[i], 10) - parseInt(segmentsB[i], 10);
+    if (diff) {
+      return diff;
+    }
+  }
+  return segmentsA.length - segmentsB.length;
+}
+
 exports.init = init
 exports.parse = parse
+exports.cmpVersions = cmpVersions
